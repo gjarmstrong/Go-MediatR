5c43ee510126aea116c2feb2f32c0ac309313cbf
Gene Armstrong <genejarmstrong@gmail.com>
2023-03-02 18:06:31 -0800

    Remove excess files

--- examples/cqrs/internal/products/features/getting_product_by_id/dtos/get_product_by_id_response.go
+++
@@ -1,9 +0,0 @@
-package dtos
-
-import (
-	"github.com/mehdihadeli/go-mediatr/examples/cqrs/internal/products/dtos"
-)
-
-type GetProductByIdQueryResponse struct {
-	Product *dtos.ProductDto `json:"product"`
-}
